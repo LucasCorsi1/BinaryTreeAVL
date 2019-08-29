9915a7b727d8cb2ba53cb83bd1a4a0454a76fdc1
LUCAS CORSI <2015200566@unibrasil.com.br>
2019-08-29 09:49:17 -0300

    Adicionar arquivos de projeto.

---
+++ BinarioSearchTree/Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarioSearchTree
+{
+    public class Node
+    {
+        public int value;
+        Node left;
+        Node right;
+
+        public Node insert(Node root, int v)
+        {
+            if (root == null)
+            {
+                root = new Node();
+                root.value = v;
+            }
+            else if (v < root.value)
+
+            {
+                root.left = insert(root.left, v);
+            }
+            else
+            {
+                root.right = insert(root.right, v);
+            }
+
+            return root;
+        }
+
+        public void traverse(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            traverse(root.left);
+            traverse(root.right);
+        }
+
+    }
+}
