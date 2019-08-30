90c80eaf7a29ad7c00b056d103eec94aa3c18cc4
LUCAS CORSI <2015200566@unibrasil.com.br>
2019-08-30 09:41:56 -0300

    concluido

--- BinarioSearchTree/Node.cs
+++ BinarioSearchTree/Node.cs
@@ -11,6 +11,39 @@
             value = d;
             left = right = null;
         }
+
+        private void inorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            inorder(root.left);
+            inorder(root.right);
+        }
+
+
+        private void preorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+
+
+        private void postorder(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+        }
     }
 }
 
