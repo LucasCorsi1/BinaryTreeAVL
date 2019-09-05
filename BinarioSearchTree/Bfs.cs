b7339c6813d448cc12df84f8a01a56e6f8ffa668
LUCAS CORSI <2015200566@unibrasil.com.br>
2019-09-05 13:37:10 -0300

    uia

---
+++ BinarioSearchTree/Bfs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarioSearchTree
+{
+
+    public class Bfs
+    {
+        InsertNodeAVL search = new InsertNodeAVL();
+
+        public bool SearchinTree(Node root, int value)
+        {
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                Node tempNode = queue.Dequeue();
+
+                if (tempNode.left != null)
+                {
+                    queue.Enqueue(tempNode.left);
+                }
+
+                if (tempNode.right != null)
+                {
+                    queue.Enqueue(tempNode.right);
+                }
+                if (tempNode.value == value) { return true; }
+
+            }
+            return false;
+
+        }
+    }
+}
