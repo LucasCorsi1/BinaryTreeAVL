fb9ae89f7fadbd63ee169c116ba26ecf111a595c
LUCAS CORSI <2015200566@unibrasil.com.br>
2019-09-05 11:51:32 -0300

    a

--- BinarioSearchTree/DeleteNode.cs
+++ BinarioSearchTree/DeleteNode.cs
@@ -8,8 +8,6 @@ namespace BinarioSearchTree
 {
     class DeleteNode
     {
-        Rasengan rodaroda = new Rasengan();
-
         public Node deleteNode(Node root, int DeleteValues) // delete
         {
             if (root == null)
@@ -74,63 +72,6 @@ namespace BinarioSearchTree
             deleteBinaryTree(root.right);
             root.right = null;
         }
-
-        /*
-
-        private void DeleteBalance(Node node, int balance)
-        {
-            while (node != null)
-            {
-                balance = (node.Balance += balance);
-
-                if (balance == 2)
-                {
-                    if (node.left.Balance >= 0)
-                    {
-                        node = rodaroda.RotateRight(node);
-
-                        if (node.Balance == -1)
-                        {
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        node =rodaroda.RotateLeftRight(node);
-                    }
-                }
-                else if (balance == -2)
-                {
-                    if (node.right.Balance <= 0)
-                    {
-                        node =rodaroda.RotateLeft(node);
-
-                        if (node.Balance == 1)
-                        {
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        node = rodaroda.RotateRightLeft(node);
-                    }
-                }
-                else if (balance != 0)
-                {
-                    return;
-                }
-
-                Node parent = node.parent;
-
-                if (parent != null)
-                {
-                    balance = parent.left == node ? -1 : 1;
-                }
-
-                node = parent;
-            }
-        }*/
-
 
     }
 
